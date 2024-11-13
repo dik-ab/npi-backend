7f5d37ce930ff647554eb8f0f3dae6dd42af68fb
dik-ab <abedaiki06@gmail.com>
2024-11-13 11:23:29 +0900

    🔧 設定ファイルの修正

--- npi/settings/local.py
+++ npi/settings/local.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
