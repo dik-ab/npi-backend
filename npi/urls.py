fd4b22e8e433144ab82f2e57de91f8063508c1cd
dik-ab <abedaiki06@gmail.com>
2024-11-14 14:41:12 +0900

    🔨 HttpResponseをインポート

--- npi/urls.py
+++ npi/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mail_templates.views import SendMailView
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
