e47971467c5a2381a5583901a44b001d869c0ca1
shota.inoue <inoue@livepass.jp>
2024-12-09 12:01:56 +0900

    URL調整_20241209

--- user_app/accounts/urls.py
+++ user_app/accounts/urls.py
@@ -4,7 +4,7 @@ from user_app.accounts.views.account_views import AccountView, PasswordResetView
 
 urlpatterns = [
     path("", AccountView.as_view(), name="account_create"),
-    path("token_generate/", PasswordResetView.as_view(), name="reset_token_generate"),
-    path("token_verify/", PasswordResetVerifyView.as_view(), name="reset_token_verify"),
+    path("password_reset_token_generate/", PasswordResetView.as_view(), name="reset_token_generate"),
+    path("password_reset_token_verify/", PasswordResetVerifyView.as_view(), name="reset_token_verify"),
     path("password_reset/", PasswordResetConfirmView.as_view(), name="reset_password"),
 ]
